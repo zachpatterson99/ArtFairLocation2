142ac4cf62378f32df783b5bdcad22414b929c7f
zachpatterson99 <zachpatterson99@gmail.com>
2024-07-05 15:50:32 -0400

    commit

---
+++ data.js
@@ -0,0 +1,53 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-location-2--liberty--ashley",
+      "name": "Location #2- Liberty & Ashley",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.14871139878993667,
+        "pitch": 0.1275754818666961,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -2.5753252747286695,
+          "pitch": 0.12205071594051731,
+          "title": "Emergency Location",
+          "text": "Blank Slate Creamery"
+        }
+      ]
+    }
+  ],
+  "name": "Location #2 Liberty & Ashley",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
